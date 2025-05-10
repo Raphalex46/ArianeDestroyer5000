9e3f038af3196136dad34daf8e0c057174a4e532
Raphaël Colin <raphael.colin68@gmail.com>
2025-05-10 23:44:10 +0200

    rewrite the minmax bot to be clearer and more flexible for parallelism

--- app/Bot/MinMax.hs
+++ app/Bot/MinMax.hs
@@ -1,4 +1,3 @@
-{-# OPTIONS_GHC -Wno-name-shadowing #-}
 {-# LANGUAGE RecordWildCards #-}
 
 -- | Implementation of a minmax algorithm
@@ -6,13 +5,13 @@ module Bot.MinMax where
 
 import Chess.Board
 import Chess.Colors
+import Chess.Coord
 import Chess.GameAnalysis
 import Chess.GameState
 import Chess.Moves
 import Chess.Pieces
 import Chess.Rules
 import Data.List
-import Chess.Coord
 
 {- | To keep things simple with the general `Bot` interface,
 we define a state even if we don't use it. It might be useful
@@ -20,6 +19,20 @@ to store information later without the need to change the whole
 interface each time.
 -}
 data MinMaxBotState = MinMaxBotState
+
+-- | Input parameters to a search.
+data SearchIn = SearchIn
+  { depth :: Int,
+    alpha :: Score,
+    beta :: Score,
+    gameState :: GameState
+  }
+
+-- | Output of a search.
+data SearchOut = SearchOut
+  { bestScore :: Score,
+    bestMove :: Move
+  }
 
 -- | The score to give to a board.
 type Score = Double
@@ -44,51 +57,53 @@ maxScore Black = -inf
 -- | Move selection algorithm for the minmax bot.
 selectMoveMinMaxBot :: MinMaxBotState -> GameState -> (Move, MinMaxBotState)
 selectMoveMinMaxBot st gs =
-  let move = snd $ minmax gs 4 (-inf) (inf)
-   in case move of
-        Nothing -> error "No move found??"
-        Just m -> (m, st)
+  let (firstMove, movesToSearch) = case getAllValidMoves gs (turn gs) of
+        [] -> error "No valid move"
+        moves@(m:_) -> (m, moves)
+      SearchOut{bestMove = move} =
+        search
+          SearchIn{depth = 4, alpha = (-inf), beta = inf, gameState = gs}
+          SearchOut{bestMove = firstMove, bestScore = minScore (turn gs)} movesToSearch
+   in (move, st)
 
--- | Minmax algorithm implementation.
-minmax :: GameState -> Int -> Score -> Score -> (Score, Maybe Move)
-minmax gs 0 a b = (eval gs, Nothing)
-minmax gs@GameState{turn = turn} n a b =
-  let moves = getAllValidMoves gs turn
-      (score, move) =
-        case moves of
-          [] -> (0.0, Nothing)
-          firstMove : _ -> let (s, m) = getBestMove (n - 1) a b (minScore turn) firstMove gs (orderMoves gs moves) in (s, return m)
-   in (score, move)
-
-{- | Find the best move by running minmax on the child moves.
-Performs alpha beta pruning (this is why I implement it as a recursive
-function as we need to be able to cutoff the search
--}
-getBestMove :: Int -> Score -> Score -> Score -> Move -> GameState -> [Move] -> (Score, Move)
-getBestMove _ _ _ res mov gs@GameState{turn = turn} [] = (res, mov)
-getBestMove n a b res mov gs@GameState{turn = turn} (curMove : ps)
-  | cutoff turn a b res = (res, mov)
+-- | Minmax with alpha-beta pruning search function.
+search :: SearchIn -> SearchOut -> [Move] -> SearchOut
+search _ searchOut [] = searchOut
+search searchIn@SearchIn{gameState = gs@GameState{turn = rootTurn}, ..} searchOut@SearchOut{..} (m : ms)
+  | cutoff rootTurn alpha beta bestScore = searchOut
   | otherwise =
-      let newAlpha = (updateAlpha turn a res)
-          newBeta = (updateBeta turn b res)
-          curState = unwrapState . playMove gs $ curMove
-          (newRes, _) = minmax curState n newAlpha newBeta
-          (bestRes, bestMove) = if (comp turn) newRes res then (newRes, curMove) else (res, mov)
-       in getBestMove n newAlpha newBeta bestRes bestMove gs ps
+      -- Consider depth 1 to be the base case
+      let moveResult = if depth == 1
+          then
+            SearchOut{bestMove=m, bestScore=eval . (unwrapState . playMove gs) $ m}
+          else
+            let curState = unwrapState . playMove gs $ m
+                movesToSearch = getAllValidMoves curState (turn curState)
+                in case movesToSearch of
+                  [] -> searchOut
+                  moves@(fm:_) -> search searchIn{depth = depth - 1, gameState = curState} SearchOut{bestMove=fm, bestScore=minScore (turn curState)} moves
+       in search (updateSearchIn searchIn moveResult) (updateSearchOut searchOut moveResult) ms
+       where
+        updateSearchIn oldIn@SearchIn{alpha=oldAlpha, beta=oldBeta} SearchOut{bestScore=resScore} =
+          oldIn{alpha = updateAlpha rootTurn oldAlpha resScore, beta = updateBeta rootTurn oldBeta resScore}
+        updateSearchOut oldOut@SearchOut{bestScore = oldBest} SearchOut{bestScore = newBest}
+          | (comp rootTurn) oldBest newBest = oldOut
+          | otherwise = SearchOut{bestScore = newBest, bestMove=m}
 
 -- | Evaluation function a board.
 eval :: GameState -> Score
 eval gs =
   let
-      matWhite = materialValue gs White
-      matBlack = materialValue gs Black
-      mat = matWhite - matBlack
-      win = winValue gs
-      check = checkValue gs
-      mob = mobilityValue gs White - mobilityValue gs Black
-      con = (controlValue gs White - matWhite * 2.0) - (controlValue gs Black - matBlack * 2.0)
-   in -- This is very much random heuristics lol
-      mat + win + 0.2 * check + con + mob
+    matWhite = materialValue gs White
+    matBlack = materialValue gs Black
+    mat = matWhite - matBlack
+    win = winValue gs
+    check = checkValue gs
+    mob = mobilityValue gs White - mobilityValue gs Black
+    con = (controlValue gs White - matWhite * 2.0) - (controlValue gs Black - matBlack * 2.0)
+   in
+    -- This is very much random heuristics lol
+    mat + win + 0.2 * check + con + mob
 
 -- | Point value for each piece.
 pieceValue :: PieceType -> Score
@@ -111,18 +126,17 @@ materialValue GameState{board = board} col =
 
 -- | Value associated to checks.
 checkValue :: GameState -> Score
-checkValue gs@GameState{board = board}
+checkValue GameState{board = board}
   | isKingInCheck board Black = 1.0
   | isKingInCheck board White = -1.0
   | otherwise = 0.0
 
 -- | Value associated to checkmates.
 winValue :: GameState -> Score
-winValue gs@GameState{board=board}
+winValue gs
   | isKingInCheckmate gs Black = inf
   | isKingInCheckmate gs White = -inf
   | otherwise = 0.0
-
 
 -- | Value for the control of squares on the board.
 controlValue :: GameState -> Color -> Score
@@ -140,6 +154,12 @@ mobilityValue gs col =
 comp :: (Ord a) => Color -> a -> a -> Bool
 comp White = (>=)
 comp Black = (<=)
+
+minmax :: Color -> [(Score, Move)] -> SearchOut
+minmax col = (optBy col) (\a b -> compare (bestScore a) (bestScore b)) . map (\(s, m) -> SearchOut{bestMove=m, bestScore=s})
+  where
+    optBy White = maximumBy
+    optBy Black = minimumBy
 
 -- | Update the alpha value.
 updateAlpha :: Color -> Score -> Score -> Score
@@ -160,15 +180,16 @@ cutoff Black a _ res = res < a
 
 -- | Move ordering function to improve alpha-beta pruning.
 orderMoves :: GameState -> [Move] -> [Move]
-orderMoves gs@GameState{..} =
+orderMoves GameState{..} =
   sortBy sortFunc
  where
   sortFunc a b
     | moveIsCapture board a && not (moveIsCapture board b) = LT
-    | otherwise = let oppKingPos = getKingCoord board (opp turn)
-                      dstA = getDstCoord board a
-                      dstB = getDstCoord board b
-                  in compare (distanceSq dstA oppKingPos) (distanceSq dstB oppKingPos)
+    | otherwise =
+        let oppKingPos = getKingCoord board (opp turn)
+            dstA = getDstCoord board a
+            dstB = getDstCoord board b
+         in compare (distanceSq dstA oppKingPos) (distanceSq dstB oppKingPos)
 
 {- | Unwrap a state from an either. Crash if an error value is encountered.
 The error case should never occur because we only get boards by playing
